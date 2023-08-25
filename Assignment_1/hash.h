fdcc13728d1d330ffececc288140df7250f02b9d
Micah Wagner <micahwagner@Micahs-MacBook-Pro.local>
2023-08-25 16:23:41 -0700

    added some functions

--- Assignment_1/hash.h
+++ Assignment_1/hash.h
@@ -19,12 +19,24 @@ int hash(char *);
 
 void hash_add(struct hash_entry *table, char *key, void *value);
 
+// rapper for list_get
+void hash_get(struct hash_entry *table, char *key, void *return_data);
+
 void check_for_rehash(struct hash_entry *table);
 
 void rehash_table(struct hash_entry *table);
 
+void hash_free(struct hash_entry *table)
+
 struct key_value *list_init();
 
-void list_add(struct key_value *list, char *key, void *value);
+void list_add(struct key_value *list_sentinel, char *key, void *value);
+
+void list_foreach(struct key_value *list, (void)(*function)(void *));
+
+// data is stored from this function into data
+void list_get(struct key_value *list, char *key, void *return_data);
+
+void listFree(struct key_value *list_sentinel);
 
 #endif
