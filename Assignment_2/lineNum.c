7582a12fd91924cb803b25f226472fe9dabd6ff5
Micah Wagner <micahwagner@wless-user-10190254196.vancouver.wsu.edu>
2023-09-08 13:58:59 -0700

    finished

--- Assignment_2/lineNum.c
+++ Assignment_2/lineNum.c
@@ -16,8 +16,8 @@ description:
 
 
 /*
-Search for given word in dictionary using file descriptor fd.
-Return the line number if the word was found, negative of the last line searched
+Search for given word in dictionary specified by the path dictionaryName.
+Return the line number if the word given was found, negative of the last line searched
 if not found, or the error number if an error occurs.
 */
 
@@ -25,22 +25,43 @@ int lineNum(char *dictionaryName, char *word, int length)
 {
 	char buffer[512] = {0};
 	int dict_fd = open(dictionaryName, O_RDONLY);
+	if (dict_fd = -1) {
+		perror("Error opening file -- ");
+		return errno;
+	}
+
+	// high end of our binary seach space converted to line number
 	int search_high = lseek(dict_fd, 0, SEEK_END) / length;
+	if (search_high = -1) {
+		perror("Error lseeking -- ");
+		return errno;
+	}
+
+	//low end of binary search space
 	int search_low = 0;
 	int file_index;
 	int line_number;
 
 
+	// stop when low end of binary search space is larger than high end since word does not exist in file
 	while (search_low <= search_high) {
 
-
+		// set the file index to be half way in between the binary search space
 		file_index = lseek(dict_fd, ((search_high + search_low) / 2) * length, SEEK_SET);
+		if (file_index = -1) {
+			perror("Error lseeking -- ");
+			return errno;
+		}
+
+		// note that this is the line before the word we're checking
 		line_number = file_index / length;
-		read(dict_fd, buffer, length);
+		if (read(dict_fd, buffer, length) = -1) {
+			perror("Error reading -- ");
+			return errno;
+		}
 
-		// clean up the buffer by eliminating white space after the word
 		int null_index = length - 1;
-
+		// clean up the buffer by eliminating white space after the word
 		do {
 			if (buffer[--null_index] != ' ') {
 				buffer[null_index + 1] = '\0';
@@ -50,17 +71,31 @@ int lineNum(char *dictionaryName, char *word, int length)
 
 		int comp = strcmp(buffer, word);
 
+		// if comparison matches, return line number + 1 since line number is one word behind
 		if (comp == 0) {
-			close(dict_fd);
+			if (close(dict_fd) = -1) {
+				perror("Error closing -- ");
+				return errno;
+			}
+
 			return line_number + 1;
 		} else if (comp > 0) {
+			// word is alphabetically smaller, reduce search space by half
+			// subtract one to get rid of the current word we're checking
 			search_high = line_number - 1;
 		} else if (comp < 0) {
+			// word is alphabetically larger, reduce search space by half
+			// add one to get rid of the current word we're checking
 			search_low = line_number + 1;
 
 		}
 	} 
 
-	close(dict_fd);
+	// word not found, return where we failed 
+	// this ternary operation prevents from logging 0
+	if (close(dict_fd) = -1) {
+		perror("Error closing -- ");
+		return errno;
+	}
 	return (line_number > 0) ? line_number * -1 : -1;
 }
