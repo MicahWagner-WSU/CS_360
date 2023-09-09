a39ed38cdb400d31a5a82d67997fdc925ee58d58
Micah Wagner <micahwagner@Micahs-MacBook-Pro.local>
2023-09-08 18:42:02 -0700

    finished

--- Assignment_2/lineNum.c
+++ Assignment_2/lineNum.c
@@ -12,6 +12,7 @@ description:
 #include <string.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 
 
@@ -24,40 +25,58 @@ if not found, or the error number if an error occurs.
 int lineNum(char *dictionaryName, char *word, int length) 
 {
 	char buffer[512] = {0};
+	int store_errno;
+	char *word_truncated = calloc(length, sizeof(char));
+	store_errno = errno;
 	int dict_fd = open(dictionaryName, O_RDONLY);
 	if (dict_fd == -1) {
 		perror("Error opening file");
-		return errno;
+		return store_errno;
 	}
 
 	// high end of our binary seach space converted to line number
-	int search_high = lseek(dict_fd, 0, SEEK_END) / length;
+	int search_high = (lseek(dict_fd, 0, SEEK_END) / length);
 	if (search_high == -1) {
 		perror("Error lseeking");
 		return errno;
 	}
 
 	//low end of binary search space
-	int search_low = 0;
+	int search_low = 1;
 	int file_index;
 	int line_number;
+
+	// truncate the given word
+	for(int i = 0; i < length - 1; i++){
+		word_truncated[i] = word[i];
+	}
+
+	word_truncated[length] = '\0';
 
 
 	// stop when low end of binary search space is larger than high end since word does not exist in file
 	while (search_low <= search_high) {
 
+		// check to see if we are getting an odd number, if so we want to round line number up
+		if ((search_high + search_low) % 2 == 1) 
+			line_number = ((search_high + search_low) / 2) + 1;
+		else
+			line_number = ((search_high + search_low) / 2);
+
 		// set the file index to be half way in between the binary search space
-		file_index = lseek(dict_fd, ((search_high + search_low) / 2) * length, SEEK_SET);
+		// we subtract by 1 because the first word sarts at lseek 0 (0 bytes in the file)
+		file_index = lseek(dict_fd, (line_number - 1) * length, SEEK_SET);
+
+		store_errno = errno;
 		if (file_index == -1) {
 			perror("Error lseeking");
-			return errno;
+			return store_errno;
 		}
 
-		// note that this is the line before the word we're checking
-		line_number = file_index / length;
 		if (read(dict_fd, buffer, length) == -1) {
+			store_errno = errno;
 			perror("Error reading");
-			return errno;
+			return store_errno;
 		}
 
 		int null_index = length - 1;
@@ -69,16 +88,17 @@ int lineNum(char *dictionaryName, char *word, int length)
 			}
 		} while (null_index);
 
-		int comp = strcmp(buffer, word);
+		int comp = strcmp(buffer, word_truncated);
 
-		// if comparison matches, return line number + 1 since line number is one word behind
+		// if comparison matches, return line number 
 		if (comp == 0) {
 			if (close(dict_fd) == -1) {
+				store_errno = errno;
 				perror("Error closing");
-				return errno;
+				return store_errno;
 			}
-
-			return line_number + 1;
+			free(word_truncated);
+			return line_number;
 		} else if (comp > 0) {
 			// word is alphabetically smaller, reduce search space by half
 			// subtract one to get rid of the current word we're checking
@@ -91,11 +111,14 @@ int lineNum(char *dictionaryName, char *word, int length)
 		}
 	} 
 
-	// word not found, return where we failed 
-	// this ternary operation prevents from logging 0
 	if (close(dict_fd) == -1) {
+		store_errno = errno;
 		perror("Error closing");
-		return errno;
+		return store_errno;
 	}
-	return (line_number > 0) ? line_number * -1 : -1;
+	
+	free(word_truncated);
+
+	// return line number * -1
+	return line_number * -1;
 }
