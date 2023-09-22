38d1604fd1d292fd9592a3faa187a6430910da9c
Micah Wagner <micahwagner@wless-user-10190255135.vancouver.wsu.edu>
2023-09-22 15:42:56 -0700

    done, I think

--- Assignment_4/assignment4.c
+++ Assignment_4/assignment4.c
@@ -24,39 +24,82 @@ name: Micah Wagner
 
 int main(int argc, char *argv[])
 {
-	int colon_index;
+	int colon_index = 0;
 	char **left;
 	char **right;
 	int fd[2];
 	int rdr, wtr;
 
+	// loop over each argv until we hit :, set colon index
 	for (int i = 0; i < argc; i++) {
 		if (strcmp(argv[i], ":") == 0) {
 			colon_index = i;
 			break;
 		}
 	}
+
+	// the left arguement starts at the arguement after argv
 	left = argv + 1;
+
+	// the right arguement starts one after the colon index
 	right = argv + colon_index + 1;
+
+	// set colon index to null
 	argv[colon_index] = NULL;
 
-	pipe(fd);
-	rdr = fd[0]; wtr = fd[1];
-	if (fork()) {
-		
-		close(wtr);
-		close(0);
-		dup(rdr); close(rdr);
-		execvp(right[0], right);
-
-	} else {
-
-		close(rdr);
-		close(1);
-		dup(wtr); close(wtr);
+	// check user never gave colon, try to run left command
+	if(colon_index == 0) {
 		execvp(argv[1], left);
+		fprintf(stderr, "%s\n", strerror(errno));
+		exit(errno);
 	}
 
-	return 0;
+	// user did give colon but not both args, run either left or right commands 
+	if(right[0] == NULL && left[0] != NULL) {
+		execvp(argv[1], left);
+		fprintf(stderr, "%s\n", strerror(errno));
+		exit(errno);
+	} else if (right[0] != NULL && left[0] == NULL) {
+		execvp(right[0], right);
+		fprintf(stderr, "%s\n", strerror(errno));
+		exit(errno);
+	// user gave no args, only a colon
+	} else if (right[0] == NULL && left[0] == NULL) {
+		fprintf(stderr, "Usage: ./assignment4 <arg1> : <arg2> \n");
+		exit(EXIT_FAILURE);
+	}
+	
+
+	// create a pipe
+	pipe(fd);
+
+	//record reader and writer 
+	rdr = fd[0]; wtr = fd[1];
+	switch (fork()) {
+		// fork failed
+		case -1:
+			fprintf(stderr, "%s\n", strerror(errno));
+			exit(errno);
+		case 0:
+			// we are child, close reader end since we're writing
+			close(rdr);
+			// close stdout and dup to connect filters
+			close(1);
+			dup(wtr); close(wtr);
+			// exec and check if failed
+			execvp(argv[1], left);
+			fprintf(stderr, "%s\n", strerror(errno));
+			exit(errno);
+		default:
+			// we are parent, close writer since we're reading
+			close(wtr);
+			// close stdin and dup to connect filters
+			close(0);
+			dup(rdr); close(rdr);
+			// exec and check if failed
+			execvp(right[0], right);
+			fprintf(stderr, "%s\n", strerror(errno));
+			exit(errno);
+	}
 }
 
