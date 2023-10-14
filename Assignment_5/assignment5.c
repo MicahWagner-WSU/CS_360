08ace2e16e7747a97e14feaf5b71b3c19ef87745
Micah Wagner <micahwagner@Micahs-MacBook-Pro.local>
2023-10-14 12:28:02 -0700

    made it better

--- Assignment_5/assignment5.c
+++ Assignment_5/assignment5.c
@@ -12,29 +12,25 @@
 /* successive calls to randomGaussian produce integer return values */
 /* having a gaussian distribution with the given mean and standard  */
 /* deviation.  Return values may be negative.                       */
-
 int randomGaussian(int mean, int stddev);
 
-void philosopher_begin_dinner(int phil_id, struct sembuf **phil_choices, int sem_ID);
+/*
+ * this function will be ran for each child process (each philosopher)
+ * we will pass in the philosopher id, 
+ * the possible choices the philosopher can make, and the sem_ID
+ */
+void philosopher_begin_dinner(int phil_id, int sem_ID);
 
-
+/*
+ * main function that creates all the philosophers (forks 5 children)
+ */
 int main(int argc, char *argv[]){
 
-	struct sembuf pick_up_chops1[2] = {{0, -1, 0}, {1, -1, 0}};
-	struct sembuf put_down_chops1[2] = {{0, 1, 0}, {1, 1, 0}};
 
-	struct sembuf pick_up_chops2[2] = {{1, -1, 0}, {2, -1, 0}};
-	struct sembuf put_down_chops2[2] = {{1, 1, 0}, {2, 1, 0}};
-
-	struct sembuf pick_up_chops3[2] = {{2, -1, 0}, {3, -1, 0}};
-	struct sembuf put_down_chops3[2] = {{2, 1, 0}, {3, 1, 0}};
-
-	struct sembuf pick_up_chops4[2] = {{3, -1, 0}, {4, -1, 0}};
-	struct sembuf put_down_chops4[2] = {{3, 1, 0}, {4, 1, 0}};
-
-	struct sembuf pick_up_chops5[2] = {{4, -1, 0}, {0, -1, 0}};
-	struct sembuf put_down_chops5[2] = {{4, 1, 0}, {0, 1, 0}};
-
+	/* 
+	 * create sembufs to set the dinner table 
+	 * (initialize semiphores to 1, like puting down the chopsticks) 
+	 */
 	struct sembuf set_table[5] = {
 		{0, 1, 0},
 		{1, 1, 0},
@@ -43,74 +39,82 @@ int main(int argc, char *argv[]){
 		{4, 1, 0}
 	};
 
+	/* create semiphores */
 	int sem_ID = semget(IPC_PRIVATE, 5, IPC_CREAT | IPC_EXCL | 0600);
 
+	/* set the dinner table */
 	semop(sem_ID, set_table, 5);
-
-	struct sembuf *philosopher_choices[10] = {
-		pick_up_chops1,
-		pick_up_chops2, 
-		pick_up_chops3, 
-		pick_up_chops4, 
-		pick_up_chops5, 
-		put_down_chops1,
-		put_down_chops2,
-		put_down_chops3,
-		put_down_chops4,
-		put_down_chops5
-	};
 
 	int errno_copy;
 
+	/* Loop and fork 5 new children */
 	for (int i = 0; i < PHIL_COUNT; i++) {
+
 		switch (fork()) {
+			/* check if fork failed*/
 			case -1:
 				errno_copy = errno;
 				perror("ERROR");
 				return errno_copy;
+			/* we are a philosopher, produce a random seed and begin dinner*/
 			case 0:
 				struct timespec time_seed;
 				clock_gettime(CLOCK_MONOTONIC, &time_seed);
 				srand(time_seed.tv_nsec);
-				philosopher_begin_dinner(i, philosopher_choices, sem_ID);
+				philosopher_begin_dinner(i, sem_ID);
 				return 0;
 
+			/* we are parent, so continue forking */
 			default:
 				break;
 
 		}
 	}
 
+	/* wait on children */
 	int status;
 	while (wait(&status) != -1);
 
-	/* code */
 	return 0;
 }
 
-void philosopher_begin_dinner(int phil_id, struct sembuf **phil_choices, int sem_ID) {
+void philosopher_begin_dinner(int phil_id, int sem_ID) {
 
 	int cycles = 0;
 	int think_time = 0;
 	int eat_time = 0;
 
+	/* 
+	 * create sembufs to pick up and put down chopsticks for each philosopher 
+	 * picking up will decrement the chopstick count for the left and right chopstick 
+	 * putting down will increment the chopstick count for the left and right chopstick 
+	 * ensure philosophers share one left and right chopstick with another philosopher 
+	 */
+	struct sembuf pick_up_chops[2] = {{phil_id, -1, 0}, {(phil_id + 1) % 5, -1, 0}};
+	struct sembuf put_down_chops[2] = {{phil_id, 1, 0}, {(phil_id + 1) % 5, 1, 0}};
+
+	/* loop until philosphers are done eating */
 	while(eat_time < 100) {
 
+		/* set time to think, then sleep for that time */
 		int time_to_think = randomGaussian(11, 7);
 		if(time_to_think < 0) time_to_think = 0;
 		printf("Philospher %d thinking for %d seconds (total = %d)\n", phil_id, time_to_think, think_time);
 		sleep(time_to_think);
 		think_time += time_to_think;
 
+		/* set time to eat, perform semop to pick up both chopsticks */
 		int time_to_eat = randomGaussian(9, 3);
 		if(time_to_eat < 0) time_to_eat = 0;
-		semop(sem_ID, phil_choices[phil_id], 2);
+		semop(sem_ID, pick_up_chops, 2);
 
+		/* sleep to simulate eat time */
 		printf("Philospher %d eating for %d seconds (total = %d)\n", phil_id, time_to_eat, eat_time);
 		sleep(time_to_eat);
 		eat_time += time_to_eat;
 
-		semop(sem_ID, phil_choices[phil_id + 5], 2);
+		/* done eating, put chopsticks back down */
+		semop(sem_ID, put_down_chops, 2);
 
 		cycles++;
 	}
